98370246265e09e7e764257ef72c26629bfcc6f7
Russell Thomas <iamgrimus@gmail.com>
2017-11-01 15:14:36 -0700

    modified server method .connect

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ let server;
 
 function runServer(databaseUrl = DATABASE_URL, port = PORT) {
   return new Promise((resolve, reject) => {
-    mongoose.createConnection(databaseUrl, err => {
+    mongoose.connection(databaseUrl, err => {
       if (err) {
         return reject(err);
       }
