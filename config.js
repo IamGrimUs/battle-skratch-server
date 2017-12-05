714b93c74bd9cad526ea0c71cef6b0fb88bc1e16
Russell Thomas <iamgrimus@gmail.com>
2017-12-04 19:15:15 -0800

    update to config file

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ exports.DATABASE_URL =
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-battle-skratch';
 exports.PORT = process.env.PORT || 8080;
-exports.JWT_SECRET = process.env.JWT_SECRET || global.JWT_SECRET;
+exports.JWT_SECRET = 'jabawakey';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '1d';
